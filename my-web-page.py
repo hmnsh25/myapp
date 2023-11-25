f12cbc5dbaa2a53e51fb3a6b7b62b1ecae509c0e
Himanshu Gupta <65725770+hmnsh25@users.noreply.github.com>
2023-11-25 19:15:08 +0530

    Update my-web-page.py

--- my-web-page.py
+++ my-web-page.py
@@ -1,7 +1,13 @@
-import streamlit as st
+import subprocess
 
-# Streamlit app title
-st.title("Hello, World!")
+req_mem = '4096'
+print("Checking Requirements to run osrm commands")
+avail = subprocess.Popen("free -m | grep Mem | awk '{print $2}'", shell=True, stdout=subprocess.PIPE).stdout.read()
+a = avail.decode()
 
-# Display "Hello, World!" on the webpage
-st.write("Hello, Himanshu!")
+if a > req_mem:
+    print("You have sufficient memory of {}".format(a))
+    exit(0)
+else:
+    print("{} is not Sufficient for Running OSRM".format(a))
+    exit(1)
